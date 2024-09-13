6baa590ef84bd6d56509ac75823ae5e2003ebff9
Vitaliy <0makovskyi@gmail.com>
2024-09-13 08:18:36 +0300

    added link navbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -2,15 +2,28 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import { useRouter } from 'next/navigation';
 
-const pages = ['Home', 'About', 'Tech Stack', 'Projects', 'Contact'];
+const pages = [
+  { title: 'Home', link: '/' },
+  { title: 'About', link: '/about' },
+  { title: 'Tech Stack', link: '/tech-stack' },
+  { title: 'projects', link: '/projects' },
+  { title: 'contact', link: '/contact' },
+];
 
 export const Navbar = () => {
+  const router = useRouter();
+
   return (
     <Box sx={{ display: 'flex', gap: '20px' }}>
       {pages.map((page) => (
-        <Button key={page} sx={{ my: 2, color: 'white', display: 'block' }}>
-          {page}
+        <Button
+          key={page.title}
+          sx={{ my: 2, color: 'white', display: 'block' }}
+          onClick={() => router.push(page.link)}
+        >
+          {page.title}
         </Button>
       ))}
     </Box>
