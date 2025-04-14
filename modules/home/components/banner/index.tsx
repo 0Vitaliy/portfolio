a70ff7441c2d757abc36fa8cd53b8d6333ec79d6
Vitalic <0makovskyi@gmail.com>
2025-04-14 12:23:33 +0300

    add form

--- modules/home/components/banner/index.tsx
+++ modules/home/components/banner/index.tsx
@@ -45,49 +45,32 @@ export const Banner = () => {
           </Box>
         </Box>
         <Box>
-        <form
-          className="elementor-form"
-          method="post"
-          name="testfind"
-          id="contact-form"
+        <form 
+            method="post" 
+            name="contact" 
+            id="contact-form"
         >
-            <div className="elementor-form-fields-wrapper elementor-labels-above">
-                <div className="elementor-field-type-text elementor-field-group elementor-column elementor-col-100">
-                    <TextField
-                        label="Name"
-                        name="form_fields[name]"
-                        type="text"
-                        className="elementor-field elementor-size-sm elementor-field-textual"
-                    />
-                </div>
-                <div className="elementor-field-type-email elementor-field-group elementor-column elementor-col-100">
-                    <TextField
-                        label="Email"
-                        name="form_fields[email]"
-                        type="email"
-                        className="elementor-field elementor-size-sm elementor-field-textual"
-                    />
-                </div>
-                <div className="elementor-field-type-textarea elementor-field-group elementor-column elementor-col-100">
-                    <TextField
-                        label="Message"
-                        name="form_fields[message]"
-                        multiline
-                        rows={4}
-                        className="elementor-field-textual elementor-field elementor-size-sm"
-                    />
-                </div>
-                <div className="elementor-field-group elementor-column elementor-field-type-submit elementor-col-100 e-form__buttons">
-                    <Button 
-                        type="submit"
-                        className="elementor-button elementor-size-sm"
-                    >
-                        <span className="elementor-button-content-wrapper">
-                            <span className="elementor-button-text">Send</span>
-                        </span>
-                    </Button>
-                </div>
-            </div>
+            <label htmlFor="name">Name</label>
+            <input 
+                type="text" 
+                name="name" 
+                id="name"
+            />
+            
+            <label htmlFor="email">Email</label>
+            <input 
+                type="email" 
+                name="email" 
+                id="email"
+            />
+            
+            <label htmlFor="message">Message</label>
+            <textarea 
+                name="message" 
+                id="message"
+            ></textarea>
+            
+            <button type="submit">Send</button>
         </form>
         </Box>
       </Wrap>
