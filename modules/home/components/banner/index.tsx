18153577683b06e0f494286509bc81bb466f916f
Vitalic <0makovskyi@gmail.com>
2025-04-14 12:48:46 +0300

    add form

--- modules/home/components/banner/index.tsx
+++ modules/home/components/banner/index.tsx
@@ -74,36 +74,6 @@ export const Banner = () => {
         </form>
     </div>
 </div>
-        <Box>
-          <form 
-              method="post" 
-              name="contact" 
-              id="contact-form"
-              data-track-form="true"
-          >
-              <label htmlFor="name">Name</label>
-              <input 
-                  type="text" 
-                  name="name" 
-                  id="name"
-              />
-              
-              <label htmlFor="email">Email</label>
-              <input 
-                  type="email" 
-                  name="email" 
-                  id="email"
-              />
-              
-              <label htmlFor="message">Message</label>
-              <textarea 
-                  name="message" 
-                  id="message"
-              ></textarea>
-              
-              <button type="submit">Send</button>
-          </form>
-        </Box>
       </Wrap>
     </Box>
   );
