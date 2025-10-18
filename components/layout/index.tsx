4c46865f94f00c88006eab70f64c1cfc9510d58a
Vitalic <0makovskyi@gmail.com>
2025-10-18 23:48:32 +0300

    added splash

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -3,6 +3,7 @@ import { Box } from "@mui/material";
 import { Header } from "./header";
 import { Footer } from "./footer";
 import { ScrollToTop } from "@/components/scroll-to-top";
+import { SplashCursor } from "@/components/splash-cursor";
 
 interface MyComponentProps {
   children: ReactNode;
@@ -19,6 +20,15 @@ export const Layout: React.FC<MyComponentProps> = ({ children }) => {
       </Box>
       <Footer />
       <ScrollToTop />
+      <SplashCursor
+        SIM_RESOLUTION={64}
+        DYE_RESOLUTION={512}
+        DENSITY_DISSIPATION={5}
+        VELOCITY_DISSIPATION={3}
+        PRESSURE_ITERATIONS={10}
+        SPLAT_RADIUS={0.15}
+        SPLAT_FORCE={4000}
+      />
     </Box>
   );
 };
